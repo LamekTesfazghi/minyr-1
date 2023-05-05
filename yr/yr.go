798e3ef139e9f45190def7e420284c06784aa0d4
LamekTesfazghi <lamekt@uia.no>
2023-05-05 01:21:49 +0000

    fikset ferdig

--- yr/yr.go
+++ yr/yr.go
@@ -1,93 +1,94 @@
 package yr
 
 import (
-	"bufio"
-	"errors"
 	"fmt"
-	"log"
-	"os"
 	"strconv"
 	"strings"
-
+	"errors"
+	"log"
+	"bufio"
+	"os"
 	"github.com/LamekTesfazghi/funtemps/conv"
 )
-
 /*
 This function counts the lines of a specified file and returns the number of lines
 */
-func countLines(filename string) float64 {
+func countLines (filename string) float64{
 	file, err := os.Open(filename) //open the specified file
-	if err != nil {                //check if there was an error opening the file
+	if err!= nil { //check if there was an error opening the file
 		log.Fatal(err)
 	}
 
 	defer file.Close()
 
-	scanner := bufio.NewScanner(file) //create a new scanner to read the file
+	scanner := bufio.NewScanner(file) //create a new scanner to read the file 
 	count := 0.0
-
-	for scanner.Scan() { //the for loop reads each line of the file using the scanner
+	
+	for scanner.Scan() { //the for loop reads each line of the file using the scanner 
 		count++ //increment the counter for each line read
 	}
 
-	if err := scanner.Err(); err != nil { //check if there was an error scanning the file
+	if err := scanner.Err(); err !=nil { //check if there was an error scanning the file
 		log.Fatal(err)
 	}
 
 	return count //return the final count
 }
 
-/*
-The function reads a file that contains weather data,
-and returns the average temperature
-*/
-func AverageT(filename string) float64 {
-	file, err := os.Open(filename)
-	if err != nil {
-		log.Fatal(err)
-	}
 
-	scanner := bufio.NewScanner(file)
-	count := 0.0
+
+/*
+The function reads a file that contains weather data, 
+and returns the average temperature  
+*/
+func AverageT (filename string) float64 {
+	file, err := os.Open(filename)
+        if err!= nil {
+                log.Fatal(err)
+        }
+
+        scanner := bufio.NewScanner(file)
+        count := 0.0
 	tempSum := 0.0
 	avg := 0.0
 
-	for scanner.Scan() {
+        for scanner.Scan() {
 		line := scanner.Text() //obtaining the recent line that was read
-
+		
 		//skip the to the next line without performing any furhter operations on the specified line
 		if strings.Contains(line, "temperatur") {
-			continue
-		}
+		continue  
+		} 
 		if strings.Contains(line, "institutt") {
-			continue
+		continue
 		}
 
 		//split up the line and get a specifif element
 		elementsInLine := strings.Split(line, ";")
 		//convert string to float64 value
 		temp, err := strconv.ParseFloat(elementsInLine[3], 64)
-		if err != nil {
+		if err!= nil {
 			log.Fatal(err)
 		}
 
 		tempSum += temp //increment the temp counter
-		count++         //increment the line counter
-	}
+                count++ //increment the line counter
+        }
 
-	if err := scanner.Err(); err != nil { //check if there was an error scanning the file
-		log.Fatal(err)
-	}
+        if err := scanner.Err(); err !=nil { //check if there was an error scanning the file
+                log.Fatal(err)
+        }
 
 	//calculate the averga
-	avg = (tempSum / count)
+	avg = (tempSum/count)
 
-	return avg
+        return avg
 }
+
 
 /*
 Takes a celsius string as input, converts it to fahrenheit using the conv package,
-and retursn the Fahr temperature in string format
+and retursn the Fahr temperature in string format 
 */
 func CelsiusToFahrenheitString(celsius string) (string, error) {
 	var fahrFloat float64
@@ -103,7 +104,7 @@ func CelsiusToFahrenheitString(celsius string) (string, error) {
 	return fahrString, err
 }
 
-/*
+/* 
 The function takes a string as input and assumes that it contains four elements seperated by ";",
 it converts the fourth element using the function declared earlier. It retruns the modified input string
 with the fourth element converted to fahrenheit
@@ -114,14 +115,14 @@ func CelsiusToFahrenheitLine(line string) (string, error) {
 	var err error
 
 	/*
-		checks if the number of elements in the slice is 4 and proceeds to the next step
-		if not it returns an error message
+	checks if the number of elements in the slice is 4 and proceeds to the next step
+	if not it returns an error message
 	*/
-	if len(elementsInLine) == 4 {
+	if (len(elementsInLine) == 4) {	
 
-		//if the line contains contains a specified string return a new string and an error
+		//if the line contains contains a specified string return a new string and an error 
 		if strings.HasPrefix(elementsInLine[0], "Data er gyldig") {
-			return "Data er basert paa gyldig data (per 18.03.2023) (CCBY 4.0) fra Meteorologisk institutt (MET);endringen er gjort av LT", err
+                return "Data er basert paa gyldig data (per 18.03.2023) (CCBY 4.0) fra Meteorologisk institutt (MET);endringen er gjort av LT", err
 		}
 
 		//converts celsius to fahrenheit and replaces the old value with the new
@@ -129,11 +130,14 @@ func CelsiusToFahrenheitLine(line string) (string, error) {
 		if err != nil {
 			return "", err
 		}
-	} else {
-		return "", errors.New("linje har ikke forventet format")
-	}
+		} else {
+			return "", errors.New("linje har ikke forventet format")
+		}
 
 	//joins the elements in the slice back into a semicolon seperated string
 	return strings.Join(elementsInLine, ";"), nil
 
+
 }
+
+
